360ac7a9f0517b60555f8b39fb6c8959000fd468
Adrian Fürschuß <adrian.fuerschuss@epunkt.net>
2017-06-23 18:19:30 +0200

    Change arguments and add paths.

--- save-windows-spotlight-lockscreens/save-spotlight-images.ps1
+++ save-windows-spotlight-lockscreens/save-spotlight-images.ps1
@@ -2,21 +2,33 @@
     # The location to save the images to.
     [Parameter(Position=1,HelpMessage="The location to save the images to.")]
     [string]
-    $savePath = "$($env:USERPROFILE)\Pictures"
+    $SavePath = "$($env:USERPROFILE)\Pictures",
+    # The location to save the robocopy log file to.
+    [Parameter(Position=2,HelpMessage="The location to save the robocopy log file to.")]
+    $LogFile = "$($env:USERPROFILE)\spotlight.log"
 )
 
-$sourceFolder = "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
-$spotlightTempFolder = $savePath
-$logFile = "C:\spotlight.log"
-robocopy $sourceFolder $spotlightTempFolder /min:102400 /log+:$logFile
+$Spotlight = "$SavePath\Spotlight"
+$SpotlightAssets = "$Spotlight\CopyAssets"
+$SpotlightHorizontal = "$Spotlight\Desktop"
+$SpotlightVertical = "$Spotlight\Mobile"
+$SpotlightLocal = "$($env:LOCALAPPDATA)\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets"
+
+Add-Type -AssemblyName System.Drawing
+New-Item "$Spotlight" -ItemType directory -Force;
+New-Item "$SpotlightAssets" -ItemType directory -Force;
+New-Item "$SpotlightHorizontal" -ItemType directory -Force;
+New-Item "$SpotlightVertical" -ItemType directory -Force;
+
+robocopy $SpotlightLocal $SpotlightAssets /min:102400 /log+:$LogFile
 #Rename all pictures
-Get-ChildItem $spotlightTempFolder | ? { -not $_.PSIsContainer } | ? { -not [System.IO.Path]::hasExtension($_)} | % {
+Get-ChildItem $SpotlightAssets | ? { -not $_.PSIsContainer } | ? { -not [System.IO.Path]::hasExtension($_)} | % {
   
   $newFileName="$($_.name).jpg"
   Rename-Item $_.FullName $newFileName -Force
   Write-Output "$_ -> $newFileName"
 }
 #Delete files without any extension
-Get-ChildItem -Path $spotlightTempFolder | Where-Object { $_.Extension -eq $null } | Remove-Item
+Get-ChildItem -Path $SpotlightAssets | Where-Object { $_.Extension -eq $null } | Remove-Item
 
 #TODO Move pictures to spotlight folder
