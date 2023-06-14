dc7e67d10f151288fa70c2cf697ddbb73abc11a9
afuersch <adrian.fuerschuss@gmx.net>
2023-06-14 16:52:49 +0200

    Update SDK and SSH packages in chocolatey.

--- chocolatey-scripts/developer_install.ps1
+++ chocolatey-scripts/developer_install.ps1
@@ -6,8 +6,9 @@ choco install git.install
 choco install docker-desktop
 
 #SDKs
-choco install jdk8
-choco install nodejs.install
+# choco install jdk8
+# choco install nodejs.install
+choco install nodejs-lts
 
 #IDEs
 choco install vscode
@@ -20,9 +21,12 @@ choco install git-fork
 #Tools
 choco install ngrok
 choco install NSwagStudio
-choco install putty
 choco install postman
 choco install azure-data-studio
+
+# SSH
+choco install putty
+choco install winscp
 
 #messenger
 choco install microsoft-teams
